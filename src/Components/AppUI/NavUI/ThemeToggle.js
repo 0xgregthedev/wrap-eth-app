96a2487c7c8ab17651da3956c8e7117cf462a5a5
gcurtis98 <40359730+gcurtis98@users.noreply.github.com>
2022-06-30 21:04:21 -0500

    added box shadow to themetoggle

--- src/Components/AppUI/NavUI/ThemeToggle.js
+++ src/Components/AppUI/NavUI/ThemeToggle.js
@@ -8,12 +8,12 @@ const ThemeToggle = (props) => {
         props.onThemeChange(cb.checked ? 'light' : 'dark')
     }
     return (
-        <div className="container">
-            <div className="toggle-switch"  onClick={toggle}>
-                <input type="checkbox" className="checkbox" name='theme' id='theme'  />
-                <label className="label" >
-                    <span className="inner" />
-                    <span className={"switch switch__" + props.theme} />
+        <div className='container'>
+            <div className='toggle-switch'  onClick={toggle}>
+                <input type='checkbox' className='checkbox' name='theme' id='theme'  />
+                <label className={'label label__' + props.theme}  >
+                    <span className='inner' />
+                    <span className={'switch switch__' + props.theme} />
                 </label>
             </div>
         </div>

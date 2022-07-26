ee10a2e7f4701e93ce5babb11d486d2dfbea88b8
gcurtis98 <40359730+gcurtis98@users.noreply.github.com>
2022-07-25 21:41:01 -0500

    final fix

--- src/Components/AppUI/NavUI/ConnectWalletBtn.js
+++ src/Components/AppUI/NavUI/ConnectWalletBtn.js
@@ -22,11 +22,18 @@ const ConnectWalletBtn = (props) => {
             props.onAddressChange(result.address ?? '');
             if (!!result.error) props.onError(result.error);
         }
-
+        const checkConnectionOnLoad = async () => {
+            let result = await window.ethereum?.selectedAddress;
+            props.onAddressChange(result ?? '');
+            setEns(await EnsLookup(result));
+            if (!!result.error) props.onError(result.error);
+        }
         if (window.ethereum) {
             window.ethereum.on('accountsChanged', accountsChangedHandler);
             window.ethereum.on('chainChanged', chainChangedHandler);
             window.ethereum.on('connect', connectHandler);
+            if (props.data.address?.length < 1)
+                checkConnectionOnLoad();
         }
         else props.onAddressChange('');
     }, [props, ens])
@@ -37,6 +44,8 @@ const ConnectWalletBtn = (props) => {
         setEns(await EnsLookup(result.address));
         if (!!result.error) props.onError(result.error);
     };
+
+
 
     if (props.data?.address?.length > 0)
         return (<button className={'wallet-btn wallet-btn__' + props.theme} >{ens?.length > 0 ? ens : (props.data.address?.substring(0, 5) + '....' + props.data.address?.substring(props.data.address?.length - 5))}</button>);
